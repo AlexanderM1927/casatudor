1deb5b8cd8bbd8139bd68edba1dea6c2fc08b721
John A. Martinez <52513621+AlexanderM1927@users.noreply.github.com>
2024-08-04 01:10:57 -0500

    Clean console

--- nuxt/helpers/ToastHelper.js
+++ nuxt/helpers/ToastHelper.js
@@ -2,7 +2,6 @@ import bootstrap from '~/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 
 export default {
     openToast (toastTarget) {
-        console.log('bootstrap', bootstrap)
         if (bootstrap) {
             const el = document.getElementById(toastTarget)
             const toastBootstrap = bootstrap.Toast.getOrCreateInstance(el)

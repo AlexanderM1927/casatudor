dacc3f70572a7397175d65e8bfa45b34544c2450
John A. Martinez <52513621+AlexanderM1927@users.noreply.github.com>
2024-08-04 01:03:35 -0500

    Fix bootstrap error

--- nuxt/helpers/ToastHelper.js
+++ nuxt/helpers/ToastHelper.js
@@ -2,6 +2,7 @@ import bootstrap from '~/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 
 export default {
     openToast (toastTarget) {
+        console.log('bootstrap', bootstrap)
         if (bootstrap) {
             const el = document.getElementById(toastTarget)
             const toastBootstrap = bootstrap.Toast.getOrCreateInstance(el)

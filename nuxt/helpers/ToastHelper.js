fa163fd9010343978d6d81b72b096f71270ed5b3
AlexanderM1927 <jhon71937@gmail.com>
2025-09-17 19:43:27 -0500

    Disabled because it is deprecated

--- nuxt/helpers/ToastHelper.js
+++ nuxt/helpers/ToastHelper.js
@@ -1,11 +1,11 @@
-import * as bootstrap from '~/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+// import * as bootstrap from '~/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 
 export default {
     openToast (toastTarget) {
         try {
-            const el = document.getElementById(toastTarget)
-            const toastBootstrap = bootstrap.Toast.getOrCreateInstance(el)
-            toastBootstrap.show()
+            // const el = document.getElementById(toastTarget)
+            // const toastBootstrap = bootstrap.Toast.getOrCreateInstance(el)
+            // toastBootstrap.show()
         } catch (error) {
             console.log('err')
         }

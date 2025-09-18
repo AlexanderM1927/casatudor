89199f87a8ce425a741fc8ba8548187cb7462cfc
AlexanderM1927 <jhon71937@gmail.com>
2025-09-17 20:51:39 -0500

    Fix new fonts to improve performance

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -15,7 +15,22 @@ export default defineNuxtConfig({
       viewport: 'width=device-width, initial-scale=1',
       htmlAttrs: {
         lang: 'es' // Add your language code here (e.g., 'en', 'es', 'fr')
-      }
+      },
+      link: [
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com'
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: ''
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
+        }
+      ]
     }
   },
   devtools: { enabled: true },

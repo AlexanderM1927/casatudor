af79e0c6c264d85298d6fa49d4af67ee6213b756
AlexanderM1927 <jhon71937@gmail.com>
2025-09-22 21:33:08 -0500

    Add safe accessors

--- nuxt/utils/orderMapper.ts
+++ nuxt/utils/orderMapper.ts
@@ -1,5 +1,6 @@
 import type { IImageStrapi } from '~/types/ImageStrapi'
 import type { IOrder } from '~/types/Order'
+import type { APICartProduct } from './cartMapper'
 
 interface APIOrder {
     id: number,
@@ -41,40 +42,40 @@ interface APIOrder {
 
 export const mapAPIOrderToIOrder = (order: APIOrder): IOrder => {
     return {
-        id: order.id,
-        address1: order.address1,
-        addresDetails: order.addresDetails,
-        city: order.city,
-        country: order.country,
-        department: order.department,
-        email: order.email,
-        phone: order.phone,
-        shipmentAgency: order.shipmentAgency,
-        shipmentGuide: order.shipmentGuide,
-        createdAt: order.createdAt,
+        id: order?.id,
+        address1: order?.address1,
+        addresDetails: order?.addresDetails,
+        city: order?.city,
+        country: order?.country,
+        department: order?.department,
+        email: order?.email,
+        phone: order?.phone,
+        shipmentAgency: order?.shipmentAgency,
+        shipmentGuide: order?.shipmentGuide,
+        createdAt: order?.createdAt,
         invoice: {
-            id: order.invoice.data.id,
-            ...order.invoice.data.attributes,
-            products: order.invoice.data.attributes.products.map((product: APICartProduct) => ({
-                id: product.product.data.id,
-                name: product.product.data.attributes.name,
-                images: product.product.data.attributes.image.data.map((el: IImageStrapi) => {
+            id: order.invoice?.data?.id,
+            ...order.invoice?.data?.attributes,
+            products: order.invoice?.data?.attributes?.products?.map((product: APICartProduct) => ({
+                id: product.product?.data?.id,
+                name: product.product?.data?.attributes?.name,
+                images: product.product?.data?.attributes?.image?.data?.map((el: IImageStrapi) => {
                     return useImageFromStrapi(el?.attributes?.url)
-                }),
-                price: product.product.data.attributes.price,
-                description: product.product.data.attributes.description,
-                price_before_offer: product.product.data.attributes.price_before_offer,
+                }) || [],
+                price: product.product?.data?.attributes?.price,
+                description: product.product?.data?.attributes?.description,
+                price_before_offer: product.product?.data?.attributes?.price_before_offer,
                 quantity: product.quantity,
                 selectedVariants: product.selectedVariants,
-            })),
+            })) || [],
         },
         user: {
-            id: order.users_permissions_user.data.id,
-            ...order.users_permissions_user.data.attributes
+            id: order.users_permissions_user?.data?.id,
+            ...order.users_permissions_user?.data?.attributes
         }
     }
 }
 
 export const mapAPIOrderToIOrders = (orders: APIOrder[]): IOrder[] => {
-    return orders.map((order) => mapAPIOrderToIOrder(order))
+    return orders?.map((order) => mapAPIOrderToIOrder(order)) || []
 }

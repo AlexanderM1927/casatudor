e5ea7d39df2a578edff102b7e2cffb31da41a5df
AlexanderM1927 <jhon71937@gmail.com>
2025-10-19 12:29:32 -0500

    footer

--- nuxt/components/TheFooter.vue
+++ nuxt/components/TheFooter.vue
@@ -1,26 +1,39 @@
 <template>
     <div class="footer">
-        <div class="footer__column">
-            <div class="company-name" @click="goToHome()">
-                <h4 class="">{{ data.companyName }}</h4>
-                <SocialMediaLinks :data="data" :color="'white'"></SocialMediaLinks>
+        <div class="footer__content">
+            <div class="footer__column">
+                <div class="company-name" @click="goToHome()">
+                    <h4 class="">{{ data.companyName }}</h4>
+                    <SocialMediaLinks :data="data" :color="'white'"></SocialMediaLinks>
+                </div>
+            </div>
+            <div class="footer__column">
+                <h4>{{ texts.links }}</h4>
+                <ul class="footer__links">
+                    <li v-for="(url, index) in data.urls" :key="index">
+                        <a :title="url.title" :href="url.url">{{ url.title }}</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer__column">
+                <h4 class="">{{ texts.contact }}</h4>
+                <p style="white-space: pre-line;">{{ data.contact }}</p>
+            </div>
+            <div class="footer__column">
+                <h4 class="">{{ data.title }}</h4>
+                <p style="white-space: pre-line;">{{ data.description }}</p>
             </div>
         </div>
-        <div class="footer__column ">
-            <h4>{{ texts.links }}</h4>
-            <ul class="footer__links">
-                <li v-for="(url, index) in data.urls" :key="index">
-                    <a :title="url.title" :href="url.url">{{ url.title }}</a>
-                </li>
-            </ul>
-        </div>
-        <div class="footer__column">
-            <h4 class="">{{ texts.contact }}</h4>
-            <p style="white-space: pre-line;">{{ data.contact }}</p>
-        </div>
-        <div class="footer__column">
-            <h4 class="">{{ data.title }}</h4>
-            <p style="white-space: pre-line;">{{ data.description }}</p>
+        <hr>
+        <div class="footer__gateways">
+            <div>
+                <b>{{ texts.pay_with }}:</b>
+                <NuxtImg
+                    :alt="texts.payment_gateways"
+                    src="/img/gateways.webp"
+                    :title="texts.payment_gateways"
+                ></NuxtImg>
+            </div>
         </div>
     </div>
 </template>
@@ -41,23 +54,32 @@ const appConfig = useRuntimeConfig();
 
 .footer {
     padding: 5rem;
-    padding-top: 7rem;
-    padding-bottom: 7rem;
     color: white;
     width: 100%;
     display: flex;
+    flex-direction: column;
+    background: $primary;
+}
+
+.footer__content {
+    display: flex;
     justify-content: center;
     gap: 5rem;
-    background: $primary;
+    width: 100%;
+}
+
+.footer__gateways {
+    display: flex;
+    justify-content: center;
 }
 
 .company-name img{
     height: 7rem;
-    width: auto;;
+    width: auto;
 }
 
 @media only screen and (max-width: $grid-breakpoints-sm) {
-    .footer {
+    .footer__content {
         flex-direction: column;
     }
 }
